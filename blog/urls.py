9b24201d719a1bf499784961a779e84ad57314a7
Alejo <alemarchan@gmail.com>
2022-09-11 18:04:11 -0300

    Subida al git

---
+++ blog/urls.py
@@ -0,0 +1,28 @@
+"""django_proyecto_cac URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import render_post, render_post_detail
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', render_post, name='post'),
+    path('<int:post_id>', render_post_detail, name='post_detail')
+]
