9224794b532408dbc8b463ba8c58aa45b77eb887
Alejo <alemarchan@gmail.com>
2022-10-07 15:20:17 -0300

    Se incluyó en el urls.py de usuarios las rutas del login (se excluyeron de la principal)

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -24,4 +25,6 @@ app_name = 'usuarios'
 
 urlpatterns = [
     path('', registro, name='registros'),
+    path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
